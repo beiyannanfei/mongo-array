5b215013938812967d0335096da4fa16caa861ea
wangyanqiang <wangyanqiang@sensoro.com>
2017-11-07 11:05:22 +0800

    update

--- app/02_objArrQuery.js
+++ app/02_objArrQuery.js
@@ -8,16 +8,37 @@ const client = require("./client.js").t2Model;
 
 function run() {
 	co(function *() {
+		const field = {_id: 0};
 		yield init();
 		//在整个嵌入/嵌套文档上的平等匹配需要指定文档的 精确匹配，包括字段顺序
-		let findResponse = yield client.find({instock: {qty: 5, warehouse: "A"}}, {_id: 0});
+		let findResponse = yield client.find({instock: {qty: 5, warehouse: "A"}}, field);
 		// console.log("====== findResponse= %j;", findResponse);
 		findResponse = [{"item": "journal", "instock": [{"warehouse": "A", "qty": 5}, {"warehouse": "C", "qty": 15}]}];
-		findResponse = yield client.find({instock: {warehouse: "A", qty: 5}}, {_id: 0});
+		findResponse = yield client.find({instock: {warehouse: "A", qty: 5}}, field);
 		// console.log("====== findResponse= %j;", findResponse);
 		findResponse = [];
 
+		//使用数组索引查询嵌入式文档中的字段
+		findResponse = yield client.find({"instock.0.qty": {$lte: 20}}, field);
+		// console.log("====== findResponse= %j;", findResponse);
+		findResponse = [
+			{"item": "journal", "instock": [{"warehouse": "A", "qty": 5}, {"warehouse": "C", "qty": 15}]},
+			{"item": "notebook", "instock": [{"warehouse": "C", "qty": 5}]},
+			{"item": "postcard", "instock": [{"warehouse": "B", "qty": 15}, {"warehouse": "C", "qty": 35}]}
+		];
 
+		// 单个嵌套文档满足嵌套字段的多个查询条件
+		findResponse = yield client.find({instock: {$elemMatch: {warehouse: "A", qty: 5}}}, field);  //<!=>{"instock.qty":5, "instock.warehouse":"A"} 该条件的查询结果会分别匹配(组合满足)
+		// console.log("====== findResponse= %j;", findResponse);
+		findResponse = [{"item": "journal", "instock": [{"warehouse": "A", "qty": 5}, {"warehouse": "C", "qty": 15}]}];
+
+		findResponse = yield client.find({instock: {$elemMatch: {qty: {$gt: 10, $lte: 20}}}}, field);
+		// console.log("====== findResponse= %j;", findResponse);
+		findResponse = [
+			{"item": "journal", "instock": [{"warehouse": "A", "qty": 5}, {"warehouse": "C", "qty": 15}]},
+			{"item": "paper", "instock": [{"warehouse": "A", "qty": 60}, {"warehouse": "B", "qty": 15}]},
+			{"item": "postcard", "instock": [{"warehouse": "B", "qty": 15}, {"warehouse": "C", "qty": 35}]}
+		];
 	}).catch(err => {
 		console.log("err: %s", err.message || err);
 	});
